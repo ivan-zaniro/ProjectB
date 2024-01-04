a9c89822167fe0cb093802c9d71e5bcc79683dec
ivans <ivans@LAPTOP-6O43JKHC>
2024-01-04 04:30:23 +0200

    implemented add()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -6,5 +6,11 @@ public class DemoB {
 		System.out.println("B11");
 		System.out.println("B11 GitHub");
 		System.out.println("B11 GitHub");
+
+	}
+
+	//Sprint 2 automation 
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 }
