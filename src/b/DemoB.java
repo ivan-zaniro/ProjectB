4af3468ab10c77377f3cee6e5dbb96b837dc85ff
ivans <ivans@LAPTOP-6O43JKHC>
2024-01-04 04:04:36 +0200

    initial commit 2

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -3,5 +3,7 @@ package b;
 public class DemoB {
 	public static void main(String[] args) {
 		System.out.println("Tuesday");
+		System.out.println("B11");
+		System.out.println("B11 GitHub");
 	}
 }
